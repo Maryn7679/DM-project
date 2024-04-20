5922a525b2ac02f61b8695df7d1c8fd750ee8d61
Maryna Kosse <mkosse@kse.org.ua>
2024-04-20 16:15:01 +0300

    Actually added generation of a random graph matrix

--- GraphGenerator.cs
+++ GraphGenerator.cs
@@ -1,6 +1,34 @@
 namespace DM_project;
 
-public class GraphGenerator
+
+public class Graph
 {
-    
+    public int[,] GraphGenerator(int vertices, int density)
+    {
+        var graph = new int[vertices, vertices];
+        var random = new Random();
+        for (var i = 0; i < vertices; i++)
+        {
+            for (var q = 0; q < vertices; q++)
+            {
+                if (i == q)
+                {
+                    graph[i, q] = 0;
+                    continue;
+                }
+
+                var edgeProbability = random.Next(100);
+                if (edgeProbability < density)
+                {
+                    graph[i, q] = random.Next(1, 1000);
+                }
+                else
+                {
+                    graph[i, q] = 0;
+                }
+            }
+        }
+
+        return graph;
+    }
 }
